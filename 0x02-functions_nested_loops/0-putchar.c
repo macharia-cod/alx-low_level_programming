decec8f2b2c03623fef5b81d27eec398903599a4
Macharia <elvismacharia09@gmail.com>
2022-09-15 12:01:04 -0700

    nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main - Prints _putchar 
+ * main - Prints _putchar
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int str[] = {95,112,117,116,99,104,97,114};
-	int count, sz;
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz; 
 
 	sz = sizeof(str) / sizeof(int);
 	for (count = 0; count < sz; count++)
